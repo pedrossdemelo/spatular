ff253514c703a9d5b6fdd1c8a8860627e0701cd1
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-18 15:58:14 -0300

    feat(utils): add function to parse Recipes

---
+++ src/utils/parseRecipe.ts
@@ -0,0 +1,253 @@
+export interface Meal {
+  idMeal: string;
+  strMeal: string;
+  strDrinkAlternate?: any;
+  strCategory: string;
+  strArea: string;
+  strInstructions: string;
+  strMealThumb: string;
+  strTags: string;
+  strYoutube: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strIngredient9: string;
+  strIngredient10: string;
+  strIngredient11: string;
+  strIngredient12: string;
+  strIngredient13: string;
+  strIngredient14: string;
+  strIngredient15: string;
+  strIngredient16: string;
+  strIngredient17: string;
+  strIngredient18: string;
+  strIngredient19: string;
+  strIngredient20: string;
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6: string;
+  strMeasure7: string;
+  strMeasure8: string;
+  strMeasure9: string;
+  strMeasure10: string;
+  strMeasure11: string;
+  strMeasure12: string;
+  strMeasure13: string;
+  strMeasure14: string;
+  strMeasure15: string;
+  strMeasure16: string;
+  strMeasure17: string;
+  strMeasure18: string;
+  strMeasure19: string;
+  strMeasure20: string;
+  strSource: string;
+  strImageSource?: any;
+  strCreativeCommonsConfirmed?: any;
+  dateModified?: any;
+}
+
+export interface Drink {
+  idDrink: string;
+  strDrink: string;
+  strDrinkAlternate?: any;
+  strTags: string;
+  strVideo: string;
+  strCategory: string;
+  strIBA: string;
+  strAlcoholic: string;
+  strGlass: string;
+  strInstructions: string;
+  strInstructionsES?: any;
+  strInstructionsDE: string;
+  strInstructionsFR?: any;
+  strInstructionsIT: string;
+  "strInstructionsZH-HANS"?: any;
+  "strInstructionsZH-HANT"?: any;
+  strDrinkThumb: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7?: any;
+  strIngredient8?: any;
+  strIngredient9?: any;
+  strIngredient10?: any;
+  strIngredient11?: any;
+  strIngredient12?: any;
+  strIngredient13?: any;
+  strIngredient14?: any;
+  strIngredient15?: any;
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6?: any;
+  strMeasure7?: any;
+  strMeasure8?: any;
+  strMeasure9?: any;
+  strMeasure10?: any;
+  strMeasure11?: any;
+  strMeasure12?: any;
+  strMeasure13?: any;
+  strMeasure14?: any;
+  strMeasure15?: any;
+  strImageSource: string;
+  strImageAttribution: string;
+  strCreativeCommonsConfirmed: string;
+  dateModified: string;
+}
+
+export default function parseRecipe(recipe: Meal & Drink) {
+  const {
+    idMeal,
+    strMeal,
+    strDrinkAlternate,
+    strCategory,
+    strArea,
+    strInstructions,
+    strMealThumb,
+    strTags,
+    strYoutube,
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10,
+    strIngredient11,
+    strIngredient12,
+    strIngredient13,
+    strIngredient14,
+    strIngredient15,
+    strIngredient16,
+    strIngredient17,
+    strIngredient18,
+    strIngredient19,
+    strIngredient20,
+    strMeasure1,
+    strMeasure2,
+    strMeasure3,
+    strMeasure4,
+    strMeasure5,
+    strMeasure6,
+    strMeasure7,
+    strMeasure8,
+    strMeasure9,
+    strMeasure10,
+    strMeasure11,
+    strMeasure12,
+    strMeasure13,
+    strMeasure14,
+    strMeasure15,
+    strMeasure16,
+    strMeasure17,
+    strMeasure18,
+    strMeasure19,
+    strMeasure20,
+    strSource,
+    strImageSource,
+    strCreativeCommonsConfirmed,
+    dateModified,
+    idDrink,
+    strDrink,
+    strVideo,
+    strIBA,
+    strAlcoholic,
+    strGlass,
+    strDrinkThumb,
+    strImageAttribution,
+  } = recipe;
+
+  const ingredientNames = [
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10,
+    strIngredient11,
+    strIngredient12,
+    strIngredient13,
+    strIngredient14,
+    strIngredient15,
+    strIngredient16,
+    strIngredient17,
+    strIngredient18,
+    strIngredient19,
+    strIngredient20,
+  ].filter((ingredient) => ingredient);
+
+  const measures = [
+    strMeasure1,
+    strMeasure2,
+    strMeasure3,
+    strMeasure4,
+    strMeasure5,
+    strMeasure6,
+    strMeasure7,
+    strMeasure8,
+    strMeasure9,
+    strMeasure10,
+    strMeasure11,
+    strMeasure12,
+    strMeasure13,
+    strMeasure14,
+    strMeasure15,
+    strMeasure16,
+    strMeasure17,
+    strMeasure18,
+    strMeasure19,
+    strMeasure20,
+  ].filter((measure) => measure);
+
+  const ingredients = ingredientNames.map((ingredient, index) => ({
+    name: ingredient,
+    measure: measures[index],
+  }));
+
+  const tags = strTags
+    ? strTags.split(",").map((tag: string) => tag.trim())
+    : [];
+
+  return {
+    id: idMeal || idDrink,
+    type: strMeal ? "food" : "drink",
+    name: strMeal || strDrink,
+    alternateName: strDrinkAlternate,
+    category: strCategory,
+    area: strArea,
+    instructions: strInstructions,
+    image: strMealThumb || strDrinkThumb,
+    youtube: strYoutube,
+    ingredients,
+    source: strSource,
+    imageSource: strImageSource,
+    creativeCommonsConfirmed: strCreativeCommonsConfirmed,
+    dateModified,
+    video: strVideo,
+    alcoholic: strAlcoholic,
+    glass: strGlass,
+    IBA: strIBA,
+    imageAttribution: strImageAttribution,
+    tags,
+  };
+}
