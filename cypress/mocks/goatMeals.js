b5225160fcb36f4f67c03e997772d46dd2c8286b
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-18 04:44:30 -0300

    test: add goatMeals mock data

---
+++ cypress/mocks/goatMeals.js
@@ -0,0 +1,12 @@
+const goatMeals = {
+  meals: [
+    {
+      strMeal: "Mbuzi Choma (Roasted Goat)",
+      strMealThumb:
+        "https://www.themealdb.com/images/media/meals/cuio7s1555492979.jpg",
+      idMeal: "52968",
+    },
+  ],
+};
+
+export default goatMeals;
