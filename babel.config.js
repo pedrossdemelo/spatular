e1fcd743396c13245724086f598a8f5bb6cfe62a
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-18 03:55:03 -0300

    build: add absolute path originating from src

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,31 @@ module.exports = (api) => {
 
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [
+            ".js",
+            ".jsx",
+            ".ts",
+            ".tsx",
+            ".android.js",
+            ".android.jsx",
+            ".android.ts",
+            ".android.tsx",
+            ".ios.js",
+            ".ios.jsx",
+            ".ios.ts",
+            ".ios.tsx",
+            ".web.js",
+            ".web.jsx",
+            ".web.ts",
+            ".web.tsx",
+          ],
+        },
+      ],
+    ],
   };
 };
