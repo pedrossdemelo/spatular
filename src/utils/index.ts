dc23c5bd00dce5d00cf45c2d0aed5568738eb6f3
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-18 15:58:46 -0300

    feat(utils): add index to utils folder

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { default as parseIngredients } from "./parseIngredients";
+export type { DrinkIngredient, MealIngredient } from "./parseIngredients";
+export { default as parseRecipe } from "./parseRecipe";
+export type { Drink, Meal } from "./parseRecipe";
