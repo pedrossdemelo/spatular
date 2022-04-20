d5ae1e3a3c2b374f2daafc99c26ee7b330ccf883
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-20 03:42:51 -0300

    feat(services): export only instances of Api classes

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,2 @@
-import TheDataDbApi from "./TheDataDbApi";
-
-export const FoodApi = Object.freeze(
-  new TheDataDbApi("meals", "https://www.themealdb.com/api/json/v1/1/"),
-);
-
-export const DrinkApi = Object.freeze(
-  new TheDataDbApi("drinks", "https://www.thecocktaildb.com/api/json/v1/1/"),
-);
+export { default as drinkApi } from "./DrinkApi";
+export { default as foodApi } from "./FoodApi";
