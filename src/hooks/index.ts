6cbba1d632e9dc493ad3f6476a6da3b17be5d66e
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-20 23:10:44 -0300

    feat(hooks): add useLocalStorage hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useDataDbApi } from "./useDataDbApi";
 export { default as useFetch } from "./useFetch";
+export { default as useLocalStorage } from "./useLocalStorage";
---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage<T>(key: string, initialValue: T) {
+  const [value, setValue] = useState<T>(initialValue);
+
+  useEffect(() => {
+    (async () => {
+      const storedValue = await AsyncStorage.getItem(key);
+
+      const parsedValue = JSON.parse(storedValue!);
+      setValue(parsedValue);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      await AsyncStorage.setItem(key, JSON.stringify(value));
+    })();
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
