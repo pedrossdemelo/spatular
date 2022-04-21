720fa48eeab9cf39f4ddb84712a1be64929f145b
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-21 01:39:41 -0300

    feat(context): add Auth Context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,38 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { createContext, useMemo } from "react";
+import { useLocalStorage } from "../hooks";
+
+type MaybeUser = null | { email: string };
+
+type AuthContextValue = readonly [
+  MaybeUser,
+  (email: string) => void,
+  () => void,
+  React.Dispatch<React.SetStateAction<MaybeUser>>,
+];
+
+export const AuthContext = createContext<AuthContextValue>([] as any);
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function AuthContextProvider({ children }: Props) {
+  const [user, setUser] = useLocalStorage<MaybeUser>("user", null);
+
+  const login = (email: string) => {
+    const newUser = { email };
+    setUser(newUser);
+  };
+
+  const logout = () => {
+    setUser(null);
+    AsyncStorage.clear();
+  };
+
+  const value = useMemo(() => [user, login, logout, setUser] as const, [user]);
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export default AuthContextProvider;
