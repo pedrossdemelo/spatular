4a79ba5c7e5ed94f04dba21d259a28a29e19adc5
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-22 10:06:06 -0300

    fix(hooks): fix loading initial state

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,12 +3,13 @@ import { useEffect, useState } from "react";
 export default function useFetch<T>(URL: string) {
   const [data, setData] = useState<T>();
 
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       try {
         const response = await fetch(URL);
         const json = await response.json();
