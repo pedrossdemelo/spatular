babca941cfa35ffa8d2e3cf93cc3d172567ed0dc
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-22 09:33:09 -0300

    feat(pages): add DrinkId page to stack

--- src/pages/Drinks/index.tsx
+++ src/pages/Drinks/index.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DrinkId from "./DrinkId";
 import Drinks from "./Drinks";
 
 type DrinksStackParamsList = {
@@ -13,6 +14,8 @@ function DrinksStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Drinks" component={Drinks} />
+
+      <Stack.Screen name="DrinkId" component={DrinkId} />
     </Stack.Navigator>
   );
 }
