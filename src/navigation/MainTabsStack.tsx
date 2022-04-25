2edf2234ce4b8ca9d54caa0aab3eca31f16009ed
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-25 09:33:21 -0300

    feat(navigation): custom tab labels and icons

--- src/navigation/MainTabsStack.tsx
+++ src/navigation/MainTabsStack.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable */
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DrinksStack from "pages/Drinks";
 import ExploreStack from "pages/Explore";
 import FoodsStack from "pages/Foods";
 import ProfileStack from "pages/Profile";
+import tw from "styles";
 
 export type MainTabsParamsList = {
   ExploreStack: undefined;
@@ -18,15 +21,68 @@ function MainTabsStack() {
     <MainTabs.Navigator
       initialRouteName="ExploreStack"
       backBehavior="history"
-      screenOptions={{ headerShown: false, lazy: false }}
+      // @ts-ignore
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        lazy: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          switch (route.name) {
+            case "ExploreStack":
+              iconName = "compass-outline";
+              break;
+            case "DrinksStack":
+              iconName = "glass-cocktail";
+              break;
+            case "FoodsStack":
+              iconName = "food-steak";
+              break;
+            case "ProfileStack":
+              iconName = "account";
+              break;
+            default:
+              iconName = "unknown";
+              break;
+          }
+
+          return (
+            <MaterialCommunityIcons
+              name={iconName as any}
+              size={size}
+              color={color}
+            />
+          );
+        },
+        tabBarActiveTintColor: tw`text-orange-600`.color,
+        tabBarInactiveTintColor: tw`text-slate-400`.color,
+        tabBarLabelStyle: tw`font-dmsans`,
+        tabBarAccessibilityLabel: "anchor",
+      })}
     >
-      <MainTabs.Screen name="ExploreStack" component={ExploreStack} />
+      <MainTabs.Screen
+        options={{ tabBarLabel: "Explore" }}
+        name="ExploreStack"
+        component={ExploreStack}
+      />
 
-      <MainTabs.Screen name="DrinksStack" component={DrinksStack} />
+      <MainTabs.Screen
+        options={{ tabBarLabel: "Drinks" }}
+        name="DrinksStack"
+        component={DrinksStack}
+      />
 
-      <MainTabs.Screen name="FoodsStack" component={FoodsStack} />
+      <MainTabs.Screen
+        options={{ tabBarLabel: "Meals" }}
+        name="FoodsStack"
+        component={FoodsStack}
+      />
 
-      <MainTabs.Screen name="ProfileStack" component={ProfileStack} />
+      <MainTabs.Screen
+        options={{ tabBarLabel: "Profile" }}
+        name="ProfileStack"
+        component={ProfileStack}
+      />
     </MainTabs.Navigator>
   );
 }
