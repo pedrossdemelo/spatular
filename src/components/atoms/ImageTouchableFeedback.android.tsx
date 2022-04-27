79a511826466f43c7e314605d4102a1597ed1ee2
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-27 19:04:27 -0300

    feat(components): add ImageTouchableFeedback

---
+++ src/components/atoms/ImageTouchableFeedback.android.tsx
@@ -0,0 +1,59 @@
+import { View } from "dripsy";
+import React, { ReactNode } from "react";
+import {
+  ColorValue,
+  ImageBackground,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+import { TouchableNativeFeedback } from "react-native-gesture-handler";
+import tw from "styles";
+import { useDeviceContext } from "twrnc";
+
+interface ImageTouchableFeedbackProps {
+  source: string;
+  children?: ReactNode;
+  sx?: StyleProp<ViewStyle>;
+  outerSx?: StyleProp<ViewStyle>;
+  pressColor?: ColorValue;
+  onPress?: () => void;
+}
+
+export default function ImageTouchableFeedback(
+  props: ImageTouchableFeedbackProps,
+) {
+  useDeviceContext(tw);
+
+  const {
+    source,
+    children,
+    onPress,
+    pressColor = tw`text-white/40 dark:text-black/40`.color,
+    sx,
+    outerSx,
+  } = props;
+
+  return (
+    <ImageBackground
+      source={{ uri: source }}
+      style={tw.style(
+        "overflow-hidden rounded-xl dark:bg-neutral-900 bg-slate-50",
+        outerSx as any,
+      )}
+      resizeMode="cover"
+    >
+      <TouchableNativeFeedback
+        background={TouchableNativeFeedback.Ripple(
+          pressColor as ColorValue,
+          false,
+        )}
+        accessibilityRole="button"
+        onPress={onPress}
+      >
+        <View sx={tw.style("min-w-full min-h-full", sx as any)}>
+          {children}
+        </View>
+      </TouchableNativeFeedback>
+    </ImageBackground>
+  );
+}
---
+++ src/components/atoms/ImageTouchableFeedback.tsx
@@ -0,0 +1,68 @@
+import { useSx, View } from "dripsy";
+import { MotiPressable } from "moti/interactions";
+import React, { ReactNode, useMemo } from "react";
+import {
+  ColorValue,
+  ImageBackground,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+import tw from "styles";
+import { useDeviceContext } from "twrnc";
+
+interface ImageTouchableFeedbackProps {
+  source: string;
+  children?: ReactNode;
+  sx?: StyleProp<ViewStyle>;
+  outerSx?: StyleProp<ViewStyle>;
+  pressColor?: ColorValue;
+  onPress?: () => void;
+}
+
+export default function ImageTouchableFeedback(
+  props: ImageTouchableFeedbackProps,
+) {
+  const {
+    source,
+    children,
+    onPress,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    pressColor,
+    sx,
+    outerSx = {},
+  } = props;
+  useDeviceContext(tw);
+
+  const parse = useSx();
+
+  return (
+    <MotiPressable
+      onPress={onPress}
+      accessibilityRole="button"
+      style={parse(
+        tw.style("rounded-xl bg-slate-50 dark:bg-neutral-900", outerSx as any),
+      )}
+      animate={useMemo(
+        () =>
+          ({ hovered, pressed }) => {
+            "worklet";
+
+            return {
+              opacity: hovered || pressed ? 0.66 : 1,
+              scale: pressed ? 0.98 : 1,
+            };
+          },
+        [],
+      )}
+      transition={{ type: "timing", duration: 100 }}
+    >
+      <ImageBackground
+        source={{ uri: source }}
+        style={parse(tw.style("w-full h-full overflow-hidden rounded-xl"))}
+        resizeMode="cover"
+      >
+        <View sx={tw.style("h-full w-full", sx as any)}>{children}</View>
+      </ImageBackground>
+    </MotiPressable>
+  );
+}
