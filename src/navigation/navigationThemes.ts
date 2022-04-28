1c57fec39a55a9ce128aea84896c7bbe27162b62
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-28 17:04:29 -0300

    refactor: stone-900 -> stone-800

--- src/navigation/navigationThemes.ts
+++ src/navigation/navigationThemes.ts
@@ -14,7 +14,7 @@ export const LightTheme = {
     primary: tw.color("orange-600")!,
     background: tw.color("stone-100")!,
     card: tw.color("stone-50")!,
-    text: tw.color("stone-800")!,
+    text: tw.color("stone-700")!,
     border: "transparent"!,
     notification: tw.color("orange-700")!,
   },
@@ -41,7 +41,7 @@ export function getStackNavigatorTheme(
     animation: "slide_from_right",
     headerTitleAlign: "center",
     headerTitleStyle: sx(
-      tw`font-dmsans font-medium text-stone-900 dark:text-neutral-200`,
+      tw`font-dmsans font-medium text-stone-800 dark:text-neutral-200`,
     ),
     headerBackTitleStyle: sx(tw`font-dmsans`),
     headerTintColor: tw.color("orange-600")!,
--- src/utils/getButtonStyles.ts
+++ src/utils/getButtonStyles.ts
@@ -23,9 +23,9 @@ export default function getButtonStyles(
           };
         case "secondary":
           return {
-            text: tw`text-stone-900 dark:text-stone-100`,
-            outer: tw`border border-stone-900 dark:border-stone-100`,
-            press: tw`text-stone-900/20 dark:text-stone-100/20`.color as string,
+            text: tw`text-stone-800 dark:text-stone-100`,
+            outer: tw`border border-stone-800 dark:border-stone-100`,
+            press: tw`text-stone-800/20 dark:text-stone-100/20`.color as string,
           };
       }
     case "contained":
@@ -38,9 +38,9 @@ export default function getButtonStyles(
           };
         case "secondary":
           return {
-            text: tw`text-stone-100 dark:text-stone-900`,
-            inner: tw`bg-stone-900 dark:bg-stone-100`,
-            press: tw`text-stone-100/20 dark:text-stone-900/20`.color as string,
+            text: tw`text-stone-100 dark:text-stone-800`,
+            inner: tw`bg-stone-800 dark:bg-stone-100`,
+            press: tw`text-stone-100/20 dark:text-stone-800/20`.color as string,
           };
       }
     case "text":
@@ -52,8 +52,8 @@ export default function getButtonStyles(
           };
         case "secondary":
           return {
-            text: tw`text-stone-900 dark:text-stone-100`,
-            press: tw`text-stone-900/20 dark:text-stone-100/20`.color as string,
+            text: tw`text-stone-800 dark:text-stone-100`,
+            press: tw`text-stone-800/20 dark:text-stone-100/20`.color as string,
           };
       }
     default:
