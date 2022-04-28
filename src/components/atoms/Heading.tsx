a197fa8e3c5b8a054aa779ef494712e38b93e4f0
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-28 13:19:43 -0300

    feat(components): heading maxWidth is now dynamic

--- src/components/atoms/Heading.tsx
+++ src/components/atoms/Heading.tsx
@@ -5,16 +5,17 @@ import { useDeviceContext } from "twrnc";
 interface HeadingProps {
   title: string;
   subtitle?: string;
+  maxWidth?: number;
 }
 
 function Heading(props: HeadingProps) {
   useDeviceContext(tw);
 
-  const { title, subtitle } = props;
+  const { title, subtitle, maxWidth = 100 } = props;
 
   return (
     <View sx={tw`items-center mx-4 pt-4`}>
-      <View sx={tw`w-full px-3 max-w-100`}>
+      <View sx={tw`w-full px-3 max-w-${maxWidth}`}>
         <Text
           sx={tw`font-dmsans mb-1 text-4xl md:text-center dark:text-neutral-200 font-medium`}
         >
