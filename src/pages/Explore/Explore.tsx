78693016f245375d45adc9a76965f07c4f1f26f9
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-27 21:25:00 -0300

    feat(pages): replace ImageBtn component

--- src/pages/Explore/Explore.tsx
+++ src/pages/Explore/Explore.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { ImageTouchableGradient } from "components/atoms";
+import { ImageDescriptionGradient } from "components/atoms";
 import { ScrollView, Text, View } from "dripsy";
 import { useDataDbApi } from "hooks";
 import React from "react";
@@ -42,43 +42,23 @@ export default function Explore() {
       <View
         sx={tw`my-4 mx-4 grow md:mb-12 md:flex-row md:mx-4 md:justify-center md:items-center`}
       >
-        <ImageTouchableGradient
+        <ImageDescriptionGradient
           onPress={goToExploreFoods}
-          sx={tw`md:w-100 w-full max-w-100 self-center aspect-1 md:mx-2`}
+          sx={tw`md:w-100 w-full max-w-100 self-center aspect-6/5 md:mx-2`}
           source={food?.image}
-        >
-          <View sx={tw`absolute bottom-4 right-5`}>
-            <Text
-              sx={tw`text-2xl font-dmsans font-medium text-white text-right`}
-            >
-              Meals
-            </Text>
-
-            <Text sx={tw`font-lato text-sm text-white text-right`}>
-              The finest culinary
-            </Text>
-          </View>
-        </ImageTouchableGradient>
+          title="Meals"
+          subtitle="The finest culinary"
+        />
 
         <View sx={tw`h-4`} />
 
-        <ImageTouchableGradient
+        <ImageDescriptionGradient
           onPress={goToExploreDrinks}
-          sx={tw`md:w-100 w-full max-w-100 self-center aspect-1 md:mx-2`}
+          sx={tw`md:w-100 w-full max-w-100 self-center aspect-6/5 md:mx-2`}
           source={drink?.image}
-        >
-          <View sx={tw`absolute bottom-4 right-5`}>
-            <Text
-              sx={tw`text-2xl font-dmsans font-medium text-white text-right`}
-            >
-              Drinks
-            </Text>
-
-            <Text sx={tw`font-lato text-sm text-white text-right`}>
-              Discover delicious flavours
-            </Text>
-          </View>
-        </ImageTouchableGradient>
+          title="Drinks"
+          subtitle="Discover delicious flavours"
+        />
       </View>
     </ScrollView>
   );
