541198ae6fa23819c162b16d83403ba382dd21bf
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-29 04:51:32 -0300

    feat(components): add FilterButton component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
+export { default as FilterButton } from "./FilterButton";
 export { default as Heading } from "./Heading";
 export { default as ImageDescriptionGradient } from "./ImageDescriptionGradient";
 export { default as ImageTouchableFeedback } from "./ImageTouchableFeedback";
