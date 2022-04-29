9a12230433498f3c76532d7c2afeac928552002a
Pedro Sousa <pedrossm2000@gmail.com>
2022-04-29 00:58:10 -0300

    refactor: add margin top to activity indicator

--- src/pages/Drinks/DrinkIdProgress.tsx
+++ src/pages/Drinks/DrinkIdProgress.tsx
@@ -3,13 +3,14 @@ import { useDataDbApi } from "hooks";
 import React from "react";
 import { ActivityIndicator } from "react-native";
 import { drinkApi } from "services";
+import tw from "styles";
 
 export default function DrinkIdProgress({ route }: any) {
   const { id } = route.params;
 
   const [[drink], loading] = useDataDbApi(drinkApi.getById(id));
 
-  if (loading) return <ActivityIndicator />;
+  if (loading) return <ActivityIndicator style={tw`mt-4`} />;
 
   return <ConsumableIdProgress data={drink} />;
 }
