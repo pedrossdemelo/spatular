47dbab7544c8ebe38c19522947fa92c5c6c70a49
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-01 05:51:58 -0300

    feat(pages): add search bar to ExploreByIngredient page

--- src/components/ExploreByIngredient.tsx
+++ src/components/ExploreByIngredient.tsx
@@ -1,38 +1,11 @@
-import { Image, Text, View } from "dripsy";
+import { Image, Text, TextInput, View } from "dripsy";
 import { useDataDbApi } from "hooks";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { ActivityIndicator, SectionList } from "react-native";
 import { drinkApi, foodApi } from "services";
 import tw from "styles";
 import { useDeviceContext } from "twrnc";
-import { parseIngredients } from "utils";
-import { Ingredient } from "utils/parseIngredients";
-
-type IngredientSection = {
-  title: string;
-  data: Ingredient[];
-};
-
-function convertToAlphabeticSections(ingredients: Ingredient[]) {
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-
-  const sections: IngredientSection[] = [];
-
-  alphabet.forEach((letter) => {
-    const filtered = ingredients.filter((ingredient) =>
-      ingredient.name.startsWith(letter),
-    );
-
-    if (filtered.length > 0) {
-      sections.push({
-        title: letter,
-        data: filtered,
-      });
-    }
-  });
-
-  return sections;
-}
+import { convertToAlphabeticalSections, parseIngredients } from "utils";
 
 interface ExploreByIngredientProps {
   type: "drink" | "food";
@@ -43,11 +16,24 @@ export default function ExploreByIngredient(props: ExploreByIngredientProps) {
 
   const Api = type === "drink" ? drinkApi : foodApi;
 
-  const [ingredients, loading] = useDataDbApi(Api.getIngredients(), {
+  const [search, setSearch] = useState("");
+
+  const [allIngredients, loading] = useDataDbApi(Api.getIngredients(), {
     parser: parseIngredients,
   });
 
-  const ingredientSections = convertToAlphabeticSections(ingredients);
+  const ingredients = useMemo(
+    () =>
+      allIngredients.filter((ingredient) =>
+        ingredient.name.toLowerCase().includes(search.toLowerCase()),
+      ),
+    [search, allIngredients],
+  );
+
+  const ingredientSections = useMemo(
+    () => convertToAlphabeticalSections(ingredients),
+    [ingredients],
+  );
 
   useDeviceContext(tw);
 
@@ -61,30 +47,42 @@ export default function ExploreByIngredient(props: ExploreByIngredientProps) {
     );
 
   return (
-    <SectionList
-      sections={ingredientSections}
-      renderItem={({ item }) => <IngredientCard data={item} />}
-      contentContainerStyle={tw`px-4`}
-      keyExtractor={(item) => item.name}
-      renderSectionHeader={({ section }) => {
-        const { title } = section;
+    <>
+      <View sx={tw`m-4 absolute top-0 right-0 left-0 z-99`}>
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search for an ingredient..."
+          textAlignVertical="center"
+          sx={tw`text-[17px] font-dmsans text-stone-800 dark:text-neutral-300 bg-stone-200 dark:bg-neutral-900 px-4 py-2.5 android:py-2 rounded-full`}
+        />
+      </View>
 
-        return (
-          <View sx={tw`flex-row items-center mx-3 mt-3 mb-2`}>
-            <Text
-              sx={tw`text-4xl font-dmsans mr-4 text-stone-600 dark:text-neutral-400 font-bold`}
-            >
-              {title}
-            </Text>
+      <SectionList
+        sections={ingredientSections}
+        renderItem={({ item }) => <IngredientCard data={item} />}
+        contentContainerStyle={tw`pt-16 px-4`}
+        keyExtractor={(item) => item.name}
+        renderSectionHeader={({ section }) => {
+          const { title } = section;
 
-            <View
-              sx={tw`flex-1 h-[3px] -mt-1.5 rounded bg-stone-200 dark:bg-neutral-900`}
-            />
-          </View>
-        );
-      }}
-      stickySectionHeadersEnabled={false}
-    />
+          return (
+            <View sx={tw`flex-row items-center mx-3 mt-3 mb-2`}>
+              <Text
+                sx={tw`text-4xl font-dmsans mr-4 text-stone-600 dark:text-neutral-400 font-bold`}
+              >
+                {title}
+              </Text>
+
+              <View
+                sx={tw`flex-1 h-[3px] -mt-1.5 rounded bg-stone-200 dark:bg-neutral-900`}
+              />
+            </View>
+          );
+        }}
+        stickySectionHeadersEnabled={false}
+      />
+    </>
   );
 }
 
