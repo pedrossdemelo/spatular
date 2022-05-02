ec0d0b1bf0631441c8bf512235613250efb5d521
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-01 21:43:39 -0300

    feat(pages): hide search bar on scrolldown in ExploreByIndex

--- src/components/ExploreByIngredient.tsx
+++ src/components/ExploreByIngredient.tsx
@@ -1,7 +1,13 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import { Image, Text, TextInput, View } from "dripsy";
 import { useDataDbApi } from "hooks";
 import React, { useMemo, useState } from "react";
 import { ActivityIndicator, SectionList } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import { drinkApi, foodApi } from "services";
 import tw from "styles";
 import { useDeviceContext } from "twrnc";
@@ -37,6 +43,16 @@ export default function ExploreByIngredient(props: ExploreByIngredientProps) {
 
   useDeviceContext(tw);
 
+  const scrollY = useSharedValue(0);
+  const scrollDirection = useSharedValue(0);
+  const translateStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: scrollDirection.value,
+      },
+    ],
+  }));
+
   if (loading)
     return (
       <ActivityIndicator
@@ -48,21 +64,51 @@ export default function ExploreByIngredient(props: ExploreByIngredientProps) {
 
   return (
     <>
-      <View sx={tw`m-4 absolute top-0 right-0 left-0 z-99`}>
+      <Animated.View
+        style={[
+          tw`absolute shadow-md justify-between top-0 border-stone-100 dark:border-neutral-800 dark:border-neutral border right-0 left-0 m-4 z-99 bg-stone-200 flex-row items-center dark:bg-neutral-900 pl-5 pr-4 py-2.5 android:py-2 rounded-full`,
+          translateStyle,
+        ]}
+      >
         <TextInput
           value={search}
           onChangeText={setSearch}
+          placeholderTextColor={
+            tw`text-stone-400 dark:text-neutral-700`.color as string
+          }
           placeholder="Search for an ingredient..."
           textAlignVertical="center"
-          sx={tw`text-[17px] font-dmsans text-stone-800 dark:text-neutral-300 bg-stone-200 dark:bg-neutral-900 px-4 py-2.5 android:py-2 rounded-full`}
+          sx={tw`text-[17px] grow font-dmsans text-stone-800 dark:text-neutral-300  py-2.5 android:py-2 -my-2.5 android:-my-2 z-99`}
         />
-      </View>
+
+        <MaterialIcons
+          name="search"
+          size={24}
+          color={tw`text-stone-400 dark:text-neutral-700`.color as string}
+        />
+      </Animated.View>
 
       <SectionList
         sections={ingredientSections}
         renderItem={({ item }) => <IngredientCard data={item} />}
         contentContainerStyle={tw`pt-16 px-4`}
         keyExtractor={(item) => item.name}
+        onScroll={({ nativeEvent }) => {
+          const { y } = nativeEvent.contentOffset;
+
+          if (Math.abs(y - scrollY.value) < 15) return;
+
+          const scrollingUp = y < scrollY.value;
+          const scrollingDown = y > scrollY.value;
+
+          if (scrollingDown && scrollDirection.value === 0)
+            scrollDirection.value = withTiming(-70);
+
+          if (scrollingUp && scrollDirection.value === -70)
+            scrollDirection.value = withTiming(0);
+
+          scrollY.value = y;
+        }}
         renderSectionHeader={({ section }) => {
           const { title } = section;
 
@@ -95,6 +141,7 @@ interface IngredientCardProps {
 
 function IngredientCard({ data }: IngredientCardProps) {
   const { image, name } = data;
+  useDeviceContext(tw);
 
   return (
     <View
