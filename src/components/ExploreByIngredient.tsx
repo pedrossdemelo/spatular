48f21c2cc8539e9cb7bf6bd0636051e3f044d8f6
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-02 01:13:35 -0300

    refactor: improve semantics

--- src/components/ExploreByIngredient.tsx
+++ src/components/ExploreByIngredient.tsx
@@ -45,11 +45,11 @@ export default function ExploreByIngredient(props: ExploreByIngredientProps) {
   useDeviceContext(tw);
 
   const scrollY = useSharedValue(0);
-  const scrollDirection = useSharedValue(0);
+  const searchBarPosition = useSharedValue(0);
   const translateStyle = useAnimatedStyle(() => ({
     transform: [
       {
-        translateY: scrollDirection.value,
+        translateY: searchBarPosition.value,
       },
     ],
   }));
@@ -99,40 +99,57 @@ export default function ExploreByIngredient(props: ExploreByIngredientProps) {
         onScroll={({ nativeEvent }) => {
           const { y } = nativeEvent.contentOffset;
 
-          if (Math.abs(y - scrollY.value) < 15) return;
+          const shouldSkip =
+            // skip when the the scroll is insignificant
+            Math.abs(y - scrollY.value) < 15 ||
+            // iOS only: skip when the scroll is outside the bounds
+            y < 0 ||
+            y >
+              nativeEvent.contentSize.height -
+                nativeEvent.layoutMeasurement.height;
 
-          const scrollingUp = y < scrollY.value;
-          const scrollingDown = y > scrollY.value;
+          if (shouldSkip) return;
 
-          if (scrollingDown && scrollDirection.value === 0)
-            scrollDirection.value = withTiming(-70);
+          const isScrollingUp = y < scrollY.value;
+          const isScrollingDown = y > scrollY.value;
 
-          if (scrollingUp && scrollDirection.value === -70)
-            scrollDirection.value = withTiming(0);
+          if (isScrollingDown && searchBarPosition.value === 0)
+            searchBarPosition.value = withTiming(-70);
+
+          if (isScrollingUp && searchBarPosition.value === -70)
+            searchBarPosition.value = withTiming(0);
 
           scrollY.value = y;
         }}
-        renderSectionHeader={({ section }) => {
-          const { title } = section;
-
-          return (
-            <View
-              sx={tw`flex-row self-center w-full max-w-134 items-center px-3 mt-3 mb-2`}
-            >
-              <Text
-                sx={tw`text-4xl font-dmsans mr-4 text-stone-600 dark:text-neutral-400 font-bold`}
-              >
-                {title}
-              </Text>
-
-              <View
-                sx={tw`flex-1 h-[3px] -mt-1.5 rounded bg-stone-200 dark:bg-neutral-900`}
-              />
-            </View>
-          );
-        }}
+        renderSectionHeader={SectionHeader}
         stickySectionHeadersEnabled={false}
       />
     </>
+  );
+}
+
+interface SectionHeaderProps {
+  section: {
+    title: string;
+  };
+}
+
+function SectionHeader({ section }: SectionHeaderProps) {
+  const { title } = section;
+
+  return (
+    <View
+      sx={tw`flex-row self-center w-full max-w-134 items-center px-3 mt-3 mb-2`}
+    >
+      <Text
+        sx={tw`text-4xl font-dmsans mr-4 text-stone-600 dark:text-neutral-400 font-bold`}
+      >
+        {title}
+      </Text>
+
+      <View
+        sx={tw`flex-1 h-[3px] -mt-1.5 rounded bg-stone-200 dark:bg-neutral-900`}
+      />
+    </View>
   );
 }
